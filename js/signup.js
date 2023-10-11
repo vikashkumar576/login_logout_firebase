b90ee03479ae9ba27dce99aad39c61c0967e4927
vikash <vikashkumar576git.com>
2023-10-11 11:35:34 +0530

    signup created

---
+++ js/signup.js
@@ -0,0 +1,28 @@
+// module 
+import { signup } from "../modules/firebase.js";
+
+
+// selectors
+const signupForm = document.querySelector("#signupForm");
+const result = document.querySelector("#result");
+
+// functions
+signupForm.onsubmit = async (e) => {
+    e.preventDefault()
+    const user = {
+        email : e.target[0].value,
+        password : e.target[1].value
+    }
+    const {success , error} = await signup(user);
+    console.log(success);
+    if (success) return (
+        result.innerHTML = "register successful !",
+        result.className = "text-green-400 font-semibold text-end capitalize",
+        signupForm.classList = "p-8 bg-white border-2 rounded-lg shadow-lg border-green-200 flex flex-col gap-3 w-[350px]"
+
+    )
+
+    result.innerHTML = "register unsuccessful !"
+    result.className = "text-red-400 font-semibold text-end capitalize"
+    signupForm.classList = "p-8 bg-white border-2 rounded-lg shadow-lg border-red-200 flex flex-col gap-3 w-[350px]"
+}
---
+++ modules/firebase.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js';
+  
+const firebaseConfig = {
+    apiKey: "AIzaSyBP4G8lTFcBtp_KZ1tdJq0PmW5KyyqtihQ",
+    authDomain: "finalfire-f3ffa.firebaseapp.com",
+    projectId: "finalfire-f3ffa",
+    storageBucket: "finalfire-f3ffa.appspot.com",
+    messagingSenderId: "958680456696",
+    appId: "1:958680456696:web:2d0d1e93113d1e8ee18a0d",
+    measurementId: "G-Y5L7JLKCK7"
+};
+
+// Initialize Firebase
+
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export const signup = async ({email , password}) => {
+
+    try {
+        const {user : {uid}} = await createUserWithEmailAndPassword(auth , email , password);
+        return {
+            success : true,
+            uid
+        } 
+
+    } 
+    catch (error) {
+        console.log(error);
+        return{
+            success : false,
+            error
+        } 
+    }
+}
+
